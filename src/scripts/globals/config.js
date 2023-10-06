46fecddb5b39c7fbebb3502c671803f0ea1eafff
Agrey Tosira <agreytosira@gmail.com>
2023-10-06 21:06:44 +0700

    feat: add drawer, fetch data, and routes

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '1c8c876a12d60a4a3c8153afcbfc575f',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us'
+}
+
+export default CONFIG
