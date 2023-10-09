e412e63ef3f4ee959afe0a2e9804be56036e176e
Agrey Tosira <agreytosira@gmail.com>
2023-10-09 20:39:49 +0700

    feat: implementing cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   KEY: '1c8c876a12d60a4a3c8153afcbfc575f',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString()
 }
 
 export default CONFIG
